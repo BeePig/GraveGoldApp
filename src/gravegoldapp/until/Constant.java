26b665e5867b9feb6166a6192b451cdb76d2f820
ongnau <beepig025@gmail.com>
2017-04-20 23:05:09 +0700

    init model

---
+++ src/gravegoldapp/until/Constant.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gravegoldapp.until;
+
+/**
+ *
+ * @author beepi
+ */
+public class Constant {
+    public static String WUMPUS = "WUMPUS";
+    public static String PIT = "PIT";
+    public static String GOLD = "GOLD";
+    public static String NEAR_WUMPUS = "NEAR WUMPUS";
+    public static String NEAR_PIT = "NEAR PIT";
+}
